5ffd9a3fb2333b43f937894362ecd38321640ba2
huydinh <huydnjs1905007@fpt.edu.vn>
2021-06-28 09:52:12 +0700

    add header + navbar + slider

---
+++ js/index.js
@@ -0,0 +1,43 @@
+let slide_index = 0
+let slide_play = true
+let slides = document.querySelectorAll('.slide')
+
+hideAllSlide = () => {
+    slides.forEach(e => {
+        e.classList.remove('active')
+    })
+}
+
+showSlide = () => {
+    hideAllSlide()
+    slides[slide_index].classList.add('active')
+}
+
+nextSlide = () => slide_index = slide_index + 1 === slides.length ? 0 : slide_index + 1
+
+prevSlide = () => slide_index = slide_index -1 < 0 ? slides.length - 1 : slide_index - 1
+
+//  pause slide when hover slider
+document.querySelector('.slider').addEventListener('mouseover', () => slide_play = false)
+
+//  enable slide when leave mouse out
+document.querySelector('.slider').addEventListener('mouseleave', () => slide_play = true)
+
+// slider control
+document.querySelector('.slide-prev').addEventListener('click',() => {
+    prevSlide()
+    showSlide()
+})
+
+document.querySelector('.slide-next').addEventListener('click',() => {
+    nextSlide()
+    showSlide()
+})
+
+showSlide()
+
+// setInterval(() => {
+//     if (!slide_play) return
+//     nextSlide()
+//     showSlide()
+// }, 1000)
